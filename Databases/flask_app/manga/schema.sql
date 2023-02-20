cfcbf96af83a92d5f30b316861495d548a25e3ae
LFK1236 <louis@falkknudsen.com>
2023-02-20 11:49:51 +0100

    Split the schema into several different files.

--- Databases/flask_app/manga/schema.sql
+++ Databases/flask_app/manga/schema.sql
@@ -1,8 +1,4 @@
-DROP TABLE IF EXISTS Volumes;
-DROP TABLE IF EXISTS Series;
-DROP TABLE IF EXISTS Authors;
-
-DROP SEQUENCE IF EXISTS Vol_ID_seq;
+\i schema_drop.sql
 
 CREATE SEQUENCE Vol_ID_seq;
 
@@ -33,12 +29,3 @@ CREATE TABLE IF NOT EXISTS Series(
     language varchar(30),
     rating integer DEFAULT 0
 );
-
-INSERT INTO Authors(name)
-VALUES ('Akira Toriyama'), ('Eiichiro Oda'), ('Hiromu Arakawa');
-
-INSERT INTO Volumes(name, publish_date, author, entry)
-VALUES  ('Dragonball', '2003-03-12', 'Akira Toriyama', 1),
-        ('Dragonball', '2003-03-12', 'Akira Toriyama', 2),
-        ('Dragonball', '2003-03-12', 'Akira Toriyama', 3)
-;
---
+++ Databases/flask_app/manga/schema_ins.sql
@@ -0,0 +1,8 @@
+INSERT INTO Authors(name)
+VALUES ('Akira Toriyama'), ('Eiichiro Oda'), ('Hiromu Arakawa');
+
+INSERT INTO Volumes(name, publish_date, author, entry)
+VALUES  ('Dragonball', '2003-03-12', 'Akira Toriyama', 1),
+        ('Dragonball', '2003-03-12', 'Akira Toriyama', 2),
+        ('Dragonball', '2003-03-12', 'Akira Toriyama', 3)
+;
