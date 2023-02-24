97868c49a5e190ec8b3e8ba5d21699a33500a8df
LFK1236 <louis@falkknudsen.com>
2023-02-24 21:28:14 +0100

    Further folder cleanup for easier overview

--- Databases/flask_app/manga/__init__.py
+++ Databases/flask_app/manga/__init__.py
@@ -5,26 +5,26 @@ app = Flask(__name__)
 database = "dbname='mangadb' user='postgres' host='127.0.0.1' password = '123'"
 connection = psycopg2.connect(database)
 
-from manga.volumes.routes import Volumes
+from manga.routes.volumes import Volumes
 app.register_blueprint(Volumes)
 
-from manga.splash.routes import Splash
+from manga.routes.splash import Splash
 app.register_blueprint(Splash)
 
-from manga.series.routes import Series
+from manga.routes.series import Series
 app.register_blueprint(Series)
 
-from manga.authors.routes import Authors
+from manga.routes.authors import Authors
 app.register_blueprint(Authors)
 
-from manga.genres.routes import Genres
+from manga.routes.genres import Genres
 app.register_blueprint(Genres)
 
-from manga.languages.routes import Languages
+from manga.routes.languages import Languages
 app.register_blueprint(Languages)
 
-from manga.demographics.routes import Demographics
+from manga.routes.demographics import Demographics
 app.register_blueprint(Demographics)
 
-from manga.publishers.routes import Publishers
+from manga.routes.publishers import Publishers
 app.register_blueprint(Publishers)
