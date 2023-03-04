4920da011f941c3f21ff40fd2d49767667ae580f
LFK1236 <louis@falkknudsen.com>
2023-03-04 11:57:09 +0100

    Expanded search options to include rating, language, demographic, and publisher

--- Databases/flask_app/manga/models/search.py
+++ Databases/flask_app/manga/models/search.py
@@ -4,16 +4,47 @@ from psycopg2 import sql
 from manga.models.series import Order_By
 from manga.models.classes import Series, Series_with_Authors
 
+def Attempt_Rating(term):
+    try:
+        return int(term)
+    except ValueError or TypeError:
+        return None
+
 def search(term, sort):
     cursor = connection.cursor()
+    vague_term = "%" + term + "%"
+    int_term = Attempt_Rating(term)
+    if (int_term != None):
+        where_rating = """
+        OR Series.rating = %s"""
+        term_list = (vague_term, term, vague_term, vague_term, vague_term, int_term)
+    else:
+        where_rating = ""
+        term_list = (vague_term, term, vague_term, vague_term, vague_term)
+
     user_sql = sql.SQL ("""
-    SELECT Series.name, Series.series_year, COUNT(entry), Series.rating FROM
-    Series LEFT JOIN Volumes ON Series.name=Volumes.name AND Series.series_year=Volumes.series_year
-    WHERE Series.name LIKE %s OR Series.series_year=%s
-    GROUP BY (Series.name, Series.series_year, Series.rating)
+    SELECT Series.name, Series.series_year, COUNT(entry), Series.rating, language, demographic, publisher
+    FROM Series
+        LEFT JOIN Volumes
+            ON Series.name=Volumes.name AND Series.series_year=Volumes.series_year
+        LEFT JOIN Language_Of
+            ON Series.name=Language_Of.series AND Series.series_year=Language_Of.series_year
+        LEFT JOIN Demographic_Of
+            ON Series.name=Demographic_Of.series AND Series.series_year=Demographic_Of.series_year
+        LEFT JOIN Publisher_Of
+            ON Series.name=Publisher_Of.series AND Series.series_year=Publisher_Of.series_year
+    WHERE Series.name LIKE %s
+        OR Series.series_year=%s
+        OR language LIKE %s
+        OR demographic LIKE %s
+        OR publisher LIKE %s
     """
+    + where_rating
+    + """
+    GROUP BY (Series.name, Series.series_year, Series.rating, language, demographic, publisher)"""
     + Order_By(sort))
-    cursor.execute(user_sql, (term, term))
+
+    cursor.execute(user_sql, term_list)
     results = cursor.fetchall()
     series = []
     for r in results:
