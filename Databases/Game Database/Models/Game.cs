631f5067ec113848be48ac43d931bce719b98a55
lfknudsen <louis@falkknudsen.com>
2023-03-18 18:00:24 +0100

    A database of owned videogames based on my flask app, but now in C# using ASP.NET

---
+++ Databases/Game Database/Models/Game.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Game_Database.Models
+{
+    public class Game
+    {
+        [Required]
+        public string? Title { get; set; }
+        [Required]
+        public string? Release { get; set; }
+    }
+}
---
+++ Databases/Game Database/Pages/Collection.cshtml.cs
@@ -0,0 +1,23 @@
+﻿using Game_Database.Services;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Game_Database.Pages
+{
+    public class CollectionModel : PageModel
+    {
+        public List<Models.Game> games = new();
+
+        private readonly ILogger<CollectionModel> _logger;
+
+        public CollectionModel(ILogger<CollectionModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+            games = GameService.GetAll();
+        }
+    }
+}
